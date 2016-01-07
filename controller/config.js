33eb21af88402f9f505ffd45c133377e632981bd
Tang,Hejun <hejun.t.tang@intel.com>
2016-01-07 17:29:42 +0800

    get list for seting page

--- controller/config.js
+++ controller/config.js
@@ -6,20 +6,7 @@ configCtrl.$inject = ['$scope', 'equipmentService']
 function configCtrl($scope, equipmentService) {
   $scope.global.curPage = 4;
 
-  $scope.equipments = equipmentService.getEquipment();
+  $scope.equipments = equipmentService.query();
 
-  $scope.edit = function(equipment) {
-    //alert(equipment.name);
-    equipment.name = "hello";
-  }
   
-  $scope.delete = function(equipment){
-    var index = 0;
-    for(var i = 0; i < $scope.equipments.length; i++) {
-      if($scope.equipments[i].name === equipment.name) {
-        index = i;
-      }
-    } 
-    $scope.equipments.splice(index, 1)
-  }
 }
